fe205cac76071a922edb0514e68291b67dda613a
YuQijaang <rdjksp@gmail.com>
2025-12-08 13:00:25 +0800

    feat: use accounts

---
+++ apps/web/app/hooks/query/use-accounts-query.ts
@@ -0,0 +1,22 @@
+import { jsonQ } from '@/utils/net'
+import { Code } from '@shared/data-transfer/_base'
+import type { AccountInfoListRespType } from '@shared/data-transfer/account/account'
+import {
+  useQuery,
+} from '@tanstack/react-query'
+export const useAccountsQuery = (isDisabled?: boolean, roles?: string[]) => {
+  const queryParams = {
+    isDisabled,
+    roles: roles?.join(','),
+  }
+  return useQuery({
+    queryKey: ['accounts', queryParams],
+    queryFn: async (): Promise<AccountInfoListRespType['data']> => {
+      const res = await jsonQ.Get<AccountInfoListRespType>('/account/account', { params: queryParams })
+      if(res.statusCode !== Code.Ok)
+        throw new Error(res.message || '获取账户列表失败')
+
+      return res.data
+    },
+  })
+}
