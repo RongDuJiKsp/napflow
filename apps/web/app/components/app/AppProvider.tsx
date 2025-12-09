a80e91543ee894f5385d6581d67a77b48ae04555
YuQiJaang <rdjksp@gmail.com>
2025-12-09 21:19:27 +0800

    fix: qc

--- apps/web/app/components/app/AppProvider.tsx
+++ apps/web/app/components/app/AppProvider.tsx
@@ -1,11 +1,17 @@
 'use client'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { App } from 'antd'
 import type { PropsWithChildren } from 'react'
 import { memo } from 'react'
+const queryClient = new QueryClient()
 
 const AppProvider = ({ children }: PropsWithChildren) => {
   return (
-    <App>{children}</App>
+    <QueryClientProvider client={queryClient}>
+      <App>
+        {children}
+      </App>
+    </QueryClientProvider>
   )
 }
 export default memo(AppProvider)
