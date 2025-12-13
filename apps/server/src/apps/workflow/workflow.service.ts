3100c8eacac28f53002d180a0a0cd8d17d130ba0
YuQiJaang <rdjksp@gmail.com>
2025-12-13 18:34:56 +0800

    feat: rt

--- apps/server/src/apps/workflow/workflow.service.ts
+++ apps/server/src/apps/workflow/workflow.service.ts
@@ -1,13 +1,33 @@
-import { Injectable } from '@nestjs/common'
-
+import { PrismaService } from '@/src/prisma/prisma.service'
+import { Inject, Injectable } from '@nestjs/common'
+import type { WorkflowAppDataType }from '@shared/data-transfer/workflow/base'
 @Injectable()
 export class WorkflowService {
-  constructor() {
+  constructor(
+    @Inject(PrismaService) private readonly prismaService: PrismaService,
+  ) {}
 
+  async createWorkflowApp(appName: string, appDesc: string) {
+    const app = await this.prismaService.workflowApp.create({
+      data: { appName, appDescription: appDesc },
+    })
+    const extraData = await this.prismaService.workflowAppData.create({
+      data: { ofAppId: app.appId },
+    })
+    return { ...app, extraData }
   }
-  
-  async createWorkflowApp(appName:string,appDesc:string){
-    
+
+  async getApp(appId: string) {
+    return await this.prismaService.workflowApp.findFirst({
+      where: { appId },
+    })
   }
-  
+
+  async loadData(appId: string) {
+    return await this.prismaService.workflowAppData.findFirst({ where: { ofAppId: appId } })
+  }
+
+  async syncData(appId: string, data: WorkflowAppDataType) {
+    return await this.prismaService.workflowAppData.update({ where: { ofAppId: appId }, data })
+  }
 }
--- packages/shared/data-transfer/workflow/base.ts
+++ packages/shared/data-transfer/workflow/base.ts
@@ -1,9 +1,12 @@
 import z from 'zod'
 
+export const WorkflowAppData = z.object({})
+export type WorkflowAppDataType = z.infer<typeof WorkflowAppData>
+
 export const WorkflowApp = z.object({
   appId: z.uuidv4(),
   appName: z.string(),
   appDescription: z.string(),
+  extraData: WorkflowAppData.nullable(),
 })
-
-export const WorkflowAppData = WorkflowApp.extend({})
+export type WorkflowAppType = z.infer<typeof WorkflowApp>
