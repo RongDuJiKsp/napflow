9fb5e067440deb6cfa74efa98e449d927bf6bc77
YuQijaang <rdjksp@gmail.com>
2025-12-19 11:34:55 +0800

    feat: 艰苦

--- apps/server/src/db/db.module.ts
+++ apps/server/src/db/db.module.ts
@@ -1,7 +1,7 @@
-import { Global, Module } from '@nestjs/common'
+import { Global, Logger, Module } from '@nestjs/common'
 import { TypeOrmModule } from '@nestjs/typeorm'
 import { AppConfigService } from '../apps/app-config/app-config.service'
-
+import mysql from 'mysql2/promise'
 import { TypeOrmService } from './typeorm.service'
 import { UserEntity, UserGroupEntity } from './models/account.entity'
 import { WorkflowAppDataEntity, WorkflowAppEntity, WorkflowAppPublishEntity } from './models/workflow.entity'
@@ -10,7 +10,17 @@ import { WorkflowAppDataEntity, WorkflowAppEntity, WorkflowAppPublishEntity } fr
 @Module({
   imports: [
     TypeOrmModule.forRootAsync({
-      useFactory(configService: AppConfigService) {
+      useFactory: async (configService: AppConfigService) => {
+        const logger = new Logger(TypeOrmService.name)
+        logger.log(`正在初始化数据库 ${configService.MYSQL_CONNECT_URL} ...`)
+        // 自动建库
+        const conn = await mysql.createConnection({
+          ...configService.sqlConnConfig,
+          multipleStatements: true,
+        })
+        await conn.query(`CREATE DATABASE IF NOT EXISTS ${configService.envs.MYSQL_DATABASE}`)
+        await conn.end()
+
         const { host, port, user, password } = configService.sqlConnConfig
         return {
           type: 'mysql',
@@ -20,14 +30,15 @@ import { WorkflowAppDataEntity, WorkflowAppEntity, WorkflowAppPublishEntity } fr
           password,
           database: configService.envs.MYSQL_DATABASE,
           synchronize: true,
-          entities: [
-            UserEntity, UserGroupEntity,
-            WorkflowAppEntity, WorkflowAppPublishEntity, WorkflowAppDataEntity,
-          ],
+          autoLoadEntities: true,
         }
       },
       inject: [AppConfigService],
     }),
+    TypeOrmModule.forFeature([
+      UserEntity, UserGroupEntity,
+      WorkflowAppEntity, WorkflowAppPublishEntity, WorkflowAppDataEntity,
+    ]),
   ],
   providers: [TypeOrmService],
   exports: [TypeOrmModule, TypeOrmService],
