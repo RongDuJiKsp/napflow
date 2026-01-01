ee1bc2e19214091c838ea110567dcb38d3c246df
YuQiJaang <rdjksp@gmail.com>
2026-01-02 00:08:34 +0800

    fix: 修复ZodSerializationExceptionFilter中的日志记录，增强错误信息的可读性

--- apps/server/src/apps/zod/middleware/zod.filter.ts
+++ apps/server/src/apps/zod/middleware/zod.filter.ts
@@ -1,6 +1,6 @@
 import type { ArgumentsHost, ExceptionFilter } from '@nestjs/common'
 import { Catch, Logger } from '@nestjs/common'
-import { ZodError } from 'zod'
+import z, { ZodError } from 'zod'
 import { Code, Resp } from '@shared/data-transfer/_base'
 import { ZodSerializationException } from 'nestjs-zod'
 import { ExpressHttpHost } from '@/src/utils/nest-middleware'
@@ -22,7 +22,7 @@ export class ZodSerializationExceptionFilter implements ExceptionFilter<ZodSeria
   private readonly logger = new Logger(ZodSerializationExceptionFilter.name)
   catch(exception: ZodSerializationException, host: ArgumentsHost) {
     const ctx = new ExpressHttpHost(host)
-    this.logger.error(`Warning: Controller(${ctx.request.path})响应校验失败(${exception.message})`)
+    this.logger.error(`Controller(${ctx.request.path})响应校验失败 \n${z.prettifyError(exception.getZodError() as ZodError)}`)
     ctx.response.status(400).json(Resp.error('Controller响应校验失败', Code.ServerError))
   }
 }
