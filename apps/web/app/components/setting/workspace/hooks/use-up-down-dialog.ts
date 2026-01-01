838ab884af836e020415e952f8a7b7641114ea0e
YuQiJaang <rdjksp@gmail.com>
2026-01-02 00:43:53 +0800

    feat: 使用ahooks的useResetState替代useState，优化选中组的状态管理

--- apps/web/app/components/setting/workspace/hooks/use-up-down-dialog.ts
+++ apps/web/app/components/setting/workspace/hooks/use-up-down-dialog.ts
@@ -1,12 +1,13 @@
 import type { UserRoleTypeType } from '@shared/data-transfer/account/base'
-import { useState } from 'react'
+import { useResetState } from 'ahooks'
 
 export const useUpDownGradeDialog = (targetUser: string, onClose: () => void, action: (target: string, groups: UserRoleTypeType[]) => void | Promise<void>) => {
-  const [selectedGroups, setSelectedGroups] = useState<UserRoleTypeType[]>([])
+  const [selectedGroups, setSelectedGroups, resetSelectedGroups] = useResetState<UserRoleTypeType[]>([])
 
   const handleConfirm = async () => {
     if (selectedGroups.length > 0) {
       await action(targetUser, selectedGroups)
+      resetSelectedGroups()
       onClose()
     }
   }
