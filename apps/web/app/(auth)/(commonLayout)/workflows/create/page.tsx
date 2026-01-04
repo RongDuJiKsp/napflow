826988b46ec8dd629fb8033c52391228e1ca9c26
YuQijaang <rdjksp@gmail.com>
2026-01-04 10:45:24 +0800

    feat: add mew

--- apps/web/app/(auth)/(commonLayout)/workflows/create/page.tsx
+++ apps/web/app/(auth)/(commonLayout)/workflows/create/page.tsx
@@ -1,7 +1,7 @@
 import CreateAppWindow from '@/app/components/workflow/create-app'
 export default function Page() {
   return (
-    <div className='h-screen w-screen flex flex-col justify-center'>
+    <div className='pt-64 w-screen flex flex-col justify-center'>
       <div className='mx-auto min-w-md'>
         <CreateAppWindow />
       </div>
--- apps/web/app/components/workflow/create-app/index.tsx
+++ apps/web/app/components/workflow/create-app/index.tsx
@@ -1,11 +1,30 @@
 'use client'
 import { memo } from 'react'
+import { Input, Label, TextArea, TextField } from '@heroui/react'
+import { twMerge } from 'tailwind-merge'
 
 const CreateAppWindow = () => {
   return (
-    <div>
-      嘻嘻哈哈
+    <div className={twMerge(
+      'p-6 bg-linear-to-br from-purple-50 to-pink-50',
+      'rounded-xl border border-pink-200 shadow-sm',
+      'max-w-md mx-auto',
+    )}>
+      <h2 className="text-xl font-semibold text-purple-700 mb-6">
+        创建新应用
+      </h2>
+      <div className="space-y-4">
+        <TextField>
+          <Label>应用名称</Label>
+          <Input maxLength={20} />
+        </TextField>
+        <TextField>
+          <Label>应用描述</Label>
+          <TextArea className={'resize-none overflow-hidden'} rows={5} maxLength={50} />
+        </TextField>
+      </div>
     </div>
   )
 }
+
 export default memo(CreateAppWindow)
