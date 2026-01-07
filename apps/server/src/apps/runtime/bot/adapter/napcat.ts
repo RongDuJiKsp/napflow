8e7b8b381487e111a1325e67b459b7c89371edb9
YuQijaang <rdjksp@gmail.com>
2026-01-07 16:23:36 +0800

    feat: 添加日志记录功能，增强信号处理和启动过程的可追踪性

--- apps/server/src/apps/runtime/bot/adapter/napcat.ts
+++ apps/server/src/apps/runtime/bot/adapter/napcat.ts
@@ -1,7 +1,9 @@
 import type { BotRecordEntity } from '@/src/apps/db/models/bot.entity'
 import type { BotAdapterFactory, BotInstance } from './_base'
+import { BotSignal } from './_base'
 import type { BotAdapter, BotState } from '@shared/data-transfer/bot/_base'
 import { AdapterTag, BotRunningState } from '@shared/data-transfer/bot/_base'
+import { Logger } from '@nestjs/common'
 
 export class NapcatWsAdapter implements BotInstance {
   static readonly adapterMeta: BotAdapter = {
@@ -12,9 +14,15 @@ export class NapcatWsAdapter implements BotInstance {
   readonly adapterDesc: string = NapcatWsAdapter.adapterMeta.adapterDesc
   readonly adapterTag: AdapterTag = NapcatWsAdapter.adapterMeta.adapterTag
   readonly botConfigDB: BotRecordEntity
+  private readonly logger: Logger
 
   constructor(db: BotRecordEntity) {
     this.botConfigDB = db
+    this.logger = new Logger(`${NapcatWsAdapter.name}-${db.name}`)
+  }
+
+  signal(signal: BotSignal) {
+    this.logger.log(`Recv signal : ${signal}(${BotSignal[signal]})`)
   }
 
   runningState(): BotState {
@@ -22,5 +30,10 @@ export class NapcatWsAdapter implements BotInstance {
       runningState: BotRunningState.stopped,
     }
   }
+
+  async bootstrap(): Promise<this> {
+    this.logger.log('Bootstrap...')
+    return this
+  }
 }
-export const NapcatWsFactory: BotAdapterFactory = db => new NapcatWsAdapter(db)
+export const NapcatWsFactory: BotAdapterFactory = async db => await new NapcatWsAdapter(db).bootstrap()
