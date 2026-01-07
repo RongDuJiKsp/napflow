5ad063bab8fbf9f831c65218346250b4a6afdebe
YuQijaang <rdjksp@gmail.com>
2026-01-07 19:37:20 +0800

    feat: 沟槽的ide 离远了不识别

--- apps/shared/data-transfer/workflow/_import.ts
+++ apps/shared/data-transfer/workflow/_import.ts
@@ -11,14 +11,15 @@ export type XYPosition = z.infer<typeof ZodCheckXYPosition>
 export const ZodCheckNode = z.looseObject({
   id: z.string(),
   position: ZodCheckXYPosition,
-  data: z.looseObject({}),
+  data: z.any(),
   type: z.enum(NodeClassic),
-})
+}).and(z.record(z.string(), z.any()))
+
 export type Node = z.infer<typeof ZodCheckNode>
 
-export const ZodCheckEdge = z.looseObject({
+export const ZodCheckEdge = z.object({
   id: z.string(),
   source: z.string(),
   target: z.string(),
-})
+}).and()
 export type Edge = z.infer<typeof ZodCheckEdge>
