639616cbfa361ff4fba9e340923b1fb76cdb46d1
YuQijaang <rdjksp@gmail.com>
2026-01-16 17:22:39 +0800

    feat: 删除

--- apps/web/app/components/workflow/editor/component-nodes/hooks/use-component-node-operations.ts
+++ apps/web/app/components/workflow/editor/component-nodes/hooks/use-component-node-operations.ts
@@ -1,16 +1,26 @@
 import { useCallback } from 'react'
 import type { ComponentNode } from '../types'
 import { ComponentNodeCreatorMap } from '../constants'
-import { useReactFlow } from '@xyflow/react'
+import { useReactFlow, useStoreApi } from '@xyflow/react'
 import { createWorkflowEdge } from '../../utils/nodes'
 
 export const useComponentNodeOperations = () => {
   const reactflow = useReactFlow()
+  const storeApi = useStoreApi()
   const handleConnenct = useCallback((source: ComponentNode, target: ComponentNode) => {
     const sourceCreator = ComponentNodeCreatorMap[source.data.type]
     const targetCreator = ComponentNodeCreatorMap[target.data.type]
     if(!sourceCreator.nextNodes?.includes(target.data.type) || !targetCreator.prevNodes?.includes(source.data.type)) return
     reactflow.addEdges(createWorkflowEdge({ source: source.id, target: target.id }))
   }, [reactflow])
-  return { handleConnenct }
+  const handleDeleteNode = useCallback((node: ComponentNode) => {
+    storeApi.setState(({ nodes, edges }) => ({
+      nodes: nodes.filter(n => n.id !== node.id),
+      edges: edges.filter(e => e.source !== node.id && e.target !== node.id),
+    }))
+  }, [storeApi])
+  return {
+    handleConnenct,
+    handleDeleteNode,
+  }
 }
--- apps/web/app/components/workflow/editor/component-nodes/mainview/node-context-menu/hooks/use-component-node-context-menu.ts
+++ apps/web/app/components/workflow/editor/component-nodes/mainview/node-context-menu/hooks/use-component-node-context-menu.ts
@@ -1,12 +1,30 @@
 import { useCallback } from 'react'
 import type { ItemParams } from 'react-contexify'
-import type { ComponentNodeProps } from '../../../types'
+import type { ComponentNode, ComponentNodeProps } from '../../../types'
+import { useComponentNodeOperations } from '../../../hooks/use-component-node-operations'
+import { useReactFlow } from '@xyflow/react'
+import type { WorkflowNode } from '../../../../types'
+import { NodeClassic } from '@shared/common/workflow/core'
+import { useWorkflowDraft } from '../../../../hooks/use-workflow-draft'
 type HandlerProps = ItemParams<ComponentNodeProps>
 
 export const useComponentNodeContextMenu = () => {
+  const reactflow = useReactFlow<WorkflowNode>()
+  const { handleDeleteNode } = useComponentNodeOperations()
+  const { submitSyncDraft } = useWorkflowDraft()
   const handleDeleteItem = useCallback(({ props }: HandlerProps) => {
-    console.log('del', props?.id)
-  }, [])
+    if(!props) {
+      console.error('Why props is undefined?')
+      return
+    }
+    const node = reactflow.getNode(props.id)
+    if(!node || node.type !== NodeClassic.Component) {
+      console.error('Why node is undefined or not a component node?')
+      return
+    }
+    handleDeleteNode(node as ComponentNode)
+    submitSyncDraft()
+  }, [handleDeleteNode, submitSyncDraft, reactflow])
   return {
     handleDeleteItem,
   }
