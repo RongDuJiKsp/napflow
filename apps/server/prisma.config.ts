44eb6ecdbbc687c6971a24934b0c245f48a7eb0d
YuQiJaang <rdjksp@gmail.com>
2026-01-17 14:06:53 +0800

    ci: prisma没了

--- apps/server/prisma.config.ts
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'prisma/config'
-export default defineConfig({
-  schema: './src/prisma',
-  datasource: {
-    // 有些时候不需要db Url ，然而cli没有url就不跑 因此用example url
-    url: process.env.DATABASE_URL ?? 'mysql://user:password@host:3306/db',
-  },
-})
