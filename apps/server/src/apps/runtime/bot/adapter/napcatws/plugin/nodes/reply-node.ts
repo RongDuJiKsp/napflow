a9611ed67fafd5a867b52c7ffcd0b143291278e2
YuQijaang <rdjksp@gmail.com>
2026-02-05 21:10:32 +0800

    feat: reply

--- apps/server/src/apps/runtime/bot/adapter/napcatws/plugin/nodes/reply-node.ts
+++ apps/server/src/apps/runtime/bot/adapter/napcatws/plugin/nodes/reply-node.ts
@@ -3,13 +3,20 @@ import type { WorkflowThread } from '@/src/apps/runtime/core/workflow/pool'
 import type { WillTask } from '@/src/utils/task-pool'
 import { Logger } from '@nestjs/common'
 import type { NapcatWsSdk } from '../../sdk'
+import { compileTemplate } from '@/src/apps/runtime/utils/templates'
+import { Structs } from '@rdjksp/node-napcat-ts'
 export class NcReplyNode extends ReplyNode {
   readonly logger = new Logger(NcReplyNode.name)
   override onThread(thread: WorkflowThread<NapcatWsSdk>, _nextTask: WillTask, nkv: Record<string, any>): void | Promise<void> {
     this.logger.debug(`Processing thread in ${thread.id}`)
     this.logger.debug(`Msg content: ${thread.kv.hmsg}`)
-    if(this.data.replyTarget === ReplyTarget.Group)
+    const content = compileTemplate(this.data.content, thread)
+    this.logger.debug(`Compiled Reply content: ${content}`)
+    if(this.data.replyTarget === ReplyTarget.Group && this.data.groupId) {
       this.logger.debug('Replying to group')
+      thread.plugin.sdk?.send_group_msg({ group_id: Number(this.data.groupId), message: [Structs.text(content)] })
+      return
+    }
 
     if(this.data.replyTarget === ReplyTarget.User)
       this.logger.debug('Replying to user')
