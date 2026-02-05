317cd37998e423935dca5a2cf0d89d2b1a7e2223
YuQijaang <rdjksp@gmail.com>
2026-02-05 18:48:46 +0800

    feat: timer

---
+++ apps/web/app/components/health-check/hooks/use-health-check.ts
@@ -0,0 +1,9 @@
+import { useHealthSamplesQuery } from '@/app/hooks/query/use-health-samples-query'
+import { useInterval } from 'ahooks'
+
+export const useHealthCheck = () => {
+  const { refetch } = useHealthSamplesQuery()
+  useInterval(() => {
+    refetch()
+  }, 60000)
+}
