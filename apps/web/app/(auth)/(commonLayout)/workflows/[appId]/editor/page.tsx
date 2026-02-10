5a569b3cc64fb01fde12af8415ca26e7c0c37713
YuQijaang <rdjksp@gmail.com>
2026-02-10 19:59:58 +0800

    feat: 拆分sider

--- apps/web/app/(auth)/(commonLayout)/workflows/[appId]/editor/page.tsx
+++ apps/web/app/(auth)/(commonLayout)/workflows/[appId]/editor/page.tsx
@@ -1,12 +1,13 @@
 import AppPublish from '@/app/components/workflow/app-publish'
 import Editor from '@/app/components/workflow/editor'
+import WorkflowSideMenus from '@/app/components/workflow/side-menus'
 
 export default async function Page() {
   return (
-    <AppPublish>
+    <WorkflowSideMenus elements={[AppPublish]}>
       <div className="w-full h-main overflow-hidden">
         <Editor />
       </div>
-    </AppPublish>
+    </WorkflowSideMenus>
   )
 }
---
+++ apps/web/app/components/workflow/side-menus/index.tsx
@@ -0,0 +1,19 @@
+import { isEqual } from 'lodash-es'
+import type { PropsWithChildren } from 'react'
+import { memo } from 'react'
+import React from 'react'
+
+const WorkflowSideMenus = ({ elements, children}: PropsWithChildren<{ elements?: React.FC[] }>) => {
+  return <div className='relative'>
+    {children}
+    <div className='absolute right-0 top-0 mt-3 mr-6 z-10'>
+      <div className='flex gap-3'>
+        {elements?.map(Element => (<Element key={Element.name}/>))}
+      </div>
+    </div>
+  </div>
+}
+
+export default memo(WorkflowSideMenus, (prevProps, nextProps) =>
+  prevProps.children === nextProps.children && isEqual(prevProps.elements, nextProps.elements),
+)
