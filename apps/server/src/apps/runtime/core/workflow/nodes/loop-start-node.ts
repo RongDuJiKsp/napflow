90aa39671e19f287e5fcc89af0b233fe6dc20e82
YuQijaang <rdjksp@gmail.com>
2026-02-26 20:29:51 +0800

    feat: loop-start

--- apps/server/src/apps/runtime/core/workflow/nodes/loop-start-node.ts
+++ apps/server/src/apps/runtime/core/workflow/nodes/loop-start-node.ts
@@ -4,8 +4,9 @@ import type { CommNodeType } from '../node'
 import { CommNode, CommNodeRole } from '../node'
 import type { WorkflowThread } from '../pool'
 import type { WillTask } from '@/src/utils/task-pool'
-import { raiseErrors } from '../../../utils/errors'
 import { LoopStartDataRawSchema } from '@shared/common/workflow/node-data/loop-start'
+import { Logger } from '@nestjs/common'
+import { merge } from 'lodash-es'
 
 // 使用 MetaSchema.extend(sharedSchema) 做兼容
 export const LoopStartDataCtxSchema = ZodCheckComponentNodeMeta.extend(
@@ -16,6 +17,7 @@ export type LoopStartDataCtx = z.infer<typeof LoopStartDataCtxSchema>
 
 export class LoopStartNode extends CommNode<LoopStartDataCtx> {
   readonly role: CommNodeRole = CommNodeRole.Action
+  private readonly logger = new Logger(LoopStartNode.name)
 
   constructor(data: CommNodeType<LoopStartDataCtx>) {
     super(data)
@@ -26,6 +28,16 @@ export class LoopStartNode extends CommNode<LoopStartDataCtx> {
     _nextTask: WillTask,
     _nkv: Record<string, any>,
   ): void | Promise<void> {
-    raiseErrors(thread, LoopStartNode)
+    // 判断一下有没有父节点
+    if(!this.parentId) {
+      this.logger.error('loop start node must have parent')
+      _nextTask.abort()
+      return
+    }
+    // 由于输出变量是写在LoopStart节点上的 因此每次执行都从Loop节点拷贝一次
+    merge(_nkv, {
+      'loop.index': thread.nodeKv[this.parentId]['loop.index'],
+      'loop.maxIndex': thread.nodeKv[this.parentId]['loop.maxIndex'],
+    })
   }
 }
