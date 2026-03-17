10aa403017311beca9f87f3e5fbb1884375eb731
YuQiJaang <rdjksp@gmail.com>
2026-03-17 15:45:42 +0800

    feat: timer

--- apps/server/src/apps/runtime/core/workflow/nodes/timer-node.ts
+++ apps/server/src/apps/runtime/core/workflow/nodes/timer-node.ts
@@ -1,5 +1,5 @@
 import { ZodCheckComponentNodeMeta } from '@shared/common/workflow/component-node'
-import { TimerDataSchema } from '@shared/common/workflow/node-data/timer'
+import { TimerDataSchema, TimerTriggerMode } from '@shared/common/workflow/node-data/timer'
 import type z from 'zod'
 import type { CommNodeType, CommTrigger } from '../node'
 import { TriggerOnEvents } from '../node'
@@ -23,8 +23,34 @@ export class TimerNode extends CommNode<TimerDataCtx> implements CommTrigger {
   onThread(
     thread: WorkflowThread,
     nextTask: WillTask,
-    nkv: Record<string, any>,
+    _nkv: Record<string, any>,
   ): void | Promise<void> {
-    throw new Error('Method not implemented.')
+        // 定时器触发时，kv里会有time、mountAt、uptime三个字段，都是unix时间戳字符串
+    const uptimeTs = Number(thread.kv.uptime)
+    const mountAtTs = Number(thread.kv.mountAt)
+    const timeTs = Number(thread.kv.time)
+    this.logger.debug(
+      `TimerNode triggered with time: ${timeTs}, mountAt: ${mountAtTs}, uptime: ${uptimeTs}`,
+    )
+    if(Number.isNaN(uptimeTs) || Number.isNaN(mountAtTs) || Number.isNaN(timeTs)) {
+      this.logger.warn('TimerNode received invalid time data, skipping trigger.')
+      nextTask.abort()
+      return
+    }
+    // 判断是否该往下走
+    const timeExpr = thread.compileEnvTemplate(this.data.timeExpr)
+    if(this.data.mode === TimerTriggerMode.Schedule) {
+      // 如果是定时触发，用当前时间和表达式算是否触发
+      const [hour, minus] = timeExpr.split(':').map(part => Number(part))
+      const currDate = new Date(timeTs * 1000)
+      if(!(currDate.getHours() === hour && currDate.getMinutes() === minus))
+        nextTask.abort()
+    }
+    else if(this.data.mode === TimerTriggerMode.Interval) {
+      // 如果是间隔触发，用uptime取模算间隔
+      const intervalSec = Number(timeExpr)
+      if(intervalSec <= 0 || uptimeTs % intervalSec !== 0)
+        nextTask.abort()
+    }
   }
 }
