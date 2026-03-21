dbc25efbf259b73b0140e8e0f80a89604861bab5
YuQiJaang <rdjksp@gmail.com>
2026-03-21 16:18:38 +0800

    feat: history

--- apps/web/app/components/workflow/editor/store/workflow-history.ts
+++ apps/web/app/components/workflow/editor/store/workflow-history.ts
@@ -1,16 +1,31 @@
 import type { Var } from '@shared/common/workflow/core/component-node'
 import type { WorkflowEdge, WorkflowNode } from '../types'
-import isDeepEqual from 'fast-deep-equal'
 import { temporal } from 'zundo'
-type WorkflowHistoryState = {
+import isDeepEqual from 'fast-deep-equal'
+
+export enum WorkflowHistoryActionTag {
+  Current = 'current', // current指的是从当前状态保存而来的
+  Programme = 'programme',
+}
+
+type WorkflowEditorHistoryState = {
   nodes: WorkflowNode[]
   edges: WorkflowEdge[]
   envs: Var[];
 }
+type WorkflowHistoryState = {
+  title?: string;
+  actionTag: WorkflowHistoryActionTag;
+} & WorkflowEditorHistoryState
+const selectEditorHistoryState = (state: WorkflowHistoryStoreShape): WorkflowEditorHistoryState => ({
+  nodes: state.nodes,
+  edges: state.edges,
+  envs: state.envs,
+})
 
 type WorkflowHistoryAction = {
-  getState: () => WorkflowHistoryState;
-  setState: (state: WorkflowHistoryState) => void;
+  getHistoryState: () => WorkflowHistoryState;
+  setHistoryState: (state: WorkflowHistoryState) => void;
 }
 
 export type WorkflowHistoryStoreShape = WorkflowHistoryState & WorkflowHistoryAction
@@ -19,10 +34,17 @@ export const createWorkflowHistoryStoreShape = temporal<WorkflowHistoryStoreShap
   nodes: [],
   edges: [],
   envs: [],
-  getState: () => ({
-    nodes: get().nodes,
-    edges: get().edges,
-    envs: get().envs,
-  }),
-  setState: (state: WorkflowHistoryState) => set(state),
-}), { equality: isDeepEqual })
+  actionTag: WorkflowHistoryActionTag.Current,
+  getHistoryState: () => get(),
+  setHistoryState: (state: WorkflowHistoryState) => set(state),
+}), {
+  // 由于无法区分每次load之前的state是用户的还是 zundo 的，所以使用深比较来判断是否真的有变化
+  equality: (past, curr) => {
+    if(curr.actionTag === WorkflowHistoryActionTag.Programme) {
+     // 如果是programme的状态 说明是覆盖action，则不进行比较，直接保存历史记录
+      return false
+    }
+    // 只有当actionTag是current时才进行比较，确保当上一次programme的状态和当前状态相同时不重复存储历史记录
+    return isDeepEqual(selectEditorHistoryState(past), selectEditorHistoryState(curr))
+  },
+})
