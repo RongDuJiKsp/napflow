4c6ac1861298bb29955f57ded41463fde3b5e418
YuQiJaang <rdjksp@gmail.com>
2026-03-26 16:11:20 +0800

    feat: invokeMsgs

--- apps/server/src/apps/agent/langchain/instance.ts
+++ apps/server/src/apps/agent/langchain/instance.ts
@@ -1,24 +1,15 @@
 import type { OpenAiEndpointConfig } from '@shared/common/agent/entity'
 import type { ClientTool, ServerTool } from '@langchain/core/tools'
-import type { AgentMiddleware } from 'langchain'
+import type { AgentMiddleware, BaseMessage } from 'langchain'
 import { HumanMessage, createAgent } from 'langchain'
 import { ChatOpenAI } from '@langchain/openai'
 import { InMemoryChatMessageHistory } from '@langchain/core/chat_history'
 
 export type LangChainConfig = { tools?: (ClientTool | ServerTool)[], middleware?: AgentMiddleware[] }
-export class LangChainInstance {
-  // models and agent
+// 对 LangChain sdk的包装，提供一些默认的功能和接口的包装，方便在 LangChainInstance 中使用
+class LangChainBase {
   private readonly openAiSdk: ChatOpenAI
   private readonly agent: ReturnType<typeof createAgent>
-
-  // memory
-  private readonly memory = new InMemoryChatMessageHistory()
-
-  private summary = ' 未摘要对话'
-
-  get chatSummary() {
-    return this.summary
-  }
 
   constructor(private readonly endpoint: OpenAiEndpointConfig, private readonly config: LangChainConfig = {}) {
     this.openAiSdk = new ChatOpenAI({
@@ -35,13 +26,30 @@ export class LangChainInstance {
     })
   }
 
+  async stateInvoke(beforeMessages: BaseMessage[]): Promise<BaseMessage[]> {
+    return (await this.agent.invoke({ messages: beforeMessages })).messages
+  }
+}
+
+export class LangChainInstance extends LangChainBase {
+  // memory
+  private readonly memory = new InMemoryChatMessageHistory()
+
+  private summary = ' 未摘要对话'
+
+  get chatSummary() {
+    return this.summary
+  }
+
   async invokeChat(input: string) {
     const history = await this.memory.getMessages()
-    const result = await this.agent.invoke({
-      messages: [...history, new HumanMessage(input)],
-    })
+    const messages = await this.stateInvoke([...history, new HumanMessage(input)])
     await this.memory.clear()
-    await this.memory.addMessages(result.messages)
+    await this.memory.addMessages(messages)
+    return{
+      current: messages,
+      diff: messages.slice(history.length), // 只返回新产生的消息
+    }
   }
 
   async getMessagesAsJson() {
