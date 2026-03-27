7e9ffc43bb5662594422a43d001c81bec4ceab57
YuQiJaang <rdjksp@gmail.com>
2026-03-27 20:23:52 +0800

    feat: logger

--- apps/server/src/apps/agent/connect/client-rpc/base.ts
+++ apps/server/src/apps/agent/connect/client-rpc/base.ts
@@ -3,8 +3,10 @@ import type { Socket } from 'socket.io'
 import z from 'zod'
 import { ClientRPCError } from '../middleware/client-rpc.filter'
 import type { RpcPS, RpcRS } from '@shared/rpc/core/ts-check'
+import { Logger } from '@nestjs/common'
 
 export class BaseClientRPCRequester extends PluginService<[Socket]> {
+  private readonly logger = new Logger(BaseClientRPCRequester.name)
   private socket: Socket | null = null
 
   mount(socket: Socket): void {
@@ -16,12 +18,14 @@ export class BaseClientRPCRequester extends PluginService<[Socket]> {
   }
 
   async emit<A extends any[], R>(...args: A): Promise<R> {
+    this.logger.debug('Emitting client RPC request', { args })
     return new Promise((resolve, reject) => {
       if (!this.socket) {
         reject(new ClientRPCError('Socket is not connected'))
         return
       }
       this.socket.emit('client-rpc', ...args, (response: R) => {
+        this.logger.debug('Received client RPC response', { response })
         resolve(response)
       })
     })
--- apps/server/src/apps/agent/connect/client-rpc/client-rpc.ts
+++ apps/server/src/apps/agent/connect/client-rpc/client-rpc.ts
@@ -2,27 +2,32 @@ import type z from 'zod'
 import type { ClientRPCHandler, ClientRPCMethods } from '@shared/rpc/agent/client-rpc/methods'
 import { CLIENT_RPC_METHODS } from '@shared/rpc/agent/client-rpc/methods'
 import { BaseClientRPCRequester } from './base'
+import { Logger } from '@nestjs/common'
 
 /**
  * @description: client rpc 即从客户端拉数据到服务端 这里是具体方法的实现类，
  * BaseLangChainClientRPCRequester 是基础类，提供了 emit 和 emitWithSchema 两个方法，前者直接发请求，后者带参数和响应的 schema 验证
  */
 export class WorkflowEditorClientRPC extends BaseClientRPCRequester {
+  private readonly callerLogger = new Logger(WorkflowEditorClientRPC.name)
   private readonly methods: ClientRPCMethods = CLIENT_RPC_METHODS
 
   getRequestSchema<K extends keyof ClientRPCMethods>(
     method: K,
   ): ClientRPCMethods[K]['request'] {
+    this.callerLogger.debug(`Getting request schema for method: ${method}`)
     return this.methods[method].request
   }
 
   getResponseSchema<K extends keyof ClientRPCMethods>(
     method: K,
   ): ClientRPCMethods[K]['response'] {
+    this.callerLogger.debug(`Getting response schema for method: ${method}`)
     return this.methods[method].response
   }
 
   getHandler<K extends keyof ClientRPCMethods>(method: K): ClientRPCHandler<K> {
+    this.callerLogger.debug(`Getting handler for method: ${method}`)
     const requestSchema = this.getRequestSchema<K>(method)
     const responseSchema = this.getResponseSchema<K>(method)
     const handler = (...args: z.input<typeof requestSchema>) => this.emitWithSchema<typeof requestSchema, typeof responseSchema>(requestSchema, responseSchema, method, ...args)
