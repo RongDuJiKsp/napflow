293f55c31aa3667474ecf95a694b0319274c6c8c
YuQiJaang <rdjksp@gmail.com>
2026-04-09 20:46:54 +0800

    fix: 沟槽的玩意

--- apps/web/e2e/login.spec.ts
+++ apps/web/e2e/login.spec.ts
@@ -3,9 +3,10 @@ import { testAi as test } from './base/midscene'
 test.describe('登录页面', () => {
   test('login 页面正常登录流程', async ({ page, aiInput, aiTap, aiAssert }) => {
     await page.goto('http://localhost/login')
-    await aiInput('邮箱地址输入框', 'root@napflow.com')
-    await aiInput('密码输入框', 'root')
+    await aiInput('root@napflow.com', '邮箱地址输入框')
+    await aiInput('root', '密码输入框')
     await aiTap('登录按钮')
+    await page.waitForTimeout(1500)
     await aiAssert('页面包含“点击开始配置您的聊天机器人”文案')
     await aiAssert('主内容区域包含“创建新Bot点击开始配置您的聊天机器人”文案')
     await aiAssert('页面标题包含“NapFlow”')
@@ -18,29 +19,33 @@ test.describe('登录页面', () => {
   test('未输入邮箱被拦截', async ({ page, aiTap, aiAssert }) => {
     await page.goto('http://localhost/login')
     await aiTap('登录按钮')
+    await page.waitForTimeout(100)
     await aiAssert('页面显示 Validation Error')
     await aiAssert('页面显示错误信息“Invalid email address”')
   })
 
   test('输入的邮箱不合法', async ({ page, aiInput, aiTap, aiAssert }) => {
     await page.goto('http://localhost/login')
-    await aiInput('邮箱地址输入框', 'napflow.com')
-    await aiInput('密码输入框', '1334')
+    await aiInput('napflow.com', '邮箱地址输入框')
+    await aiInput('1334', '密码输入框')
     await aiTap('登录按钮')
+    await page.waitForTimeout(100)
     await aiAssert('页面显示错误信息“Invalid email address”')
   })
 
   test('未输入密码被拦截', async ({ page, aiTap, aiAssert }) => {
     await page.goto('http://localhost/login')
     await aiTap('登录按钮')
+    await page.waitForTimeout(100)
     await aiAssert('页面显示密码必填错误信息')
   })
 
   test('前端校验全部通过 但是用户不存在', async ({ page, aiInput, aiTap, aiAssert }) => {
     await page.goto('http://localhost/login')
-    await aiInput('邮箱地址输入框', 'riit@napflow.com')
-    await aiInput('密码输入框', '134345')
+    await aiInput('riit@napflow.com', '邮箱地址输入框')
+    await aiInput('134345', '密码输入框')
     await aiTap('登录按钮')
+    await page.waitForTimeout(100)
     await aiAssert('页面显示“用户不存在或密码错误”')
   })
 })
